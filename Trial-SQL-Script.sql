7d77c61a43345df9a8b729d52c1963eb4dff1f8b
max-student <max-student@maxtrain.com>
2020-02-10 13:44:05 -0500

    current

--- Trial-SQL-Script.sql
+++ Trial-SQL-Script.sql
@@ -7,23 +7,115 @@ Location nvarchar(20) not null,
 SuperBowlMVP nvarchar(80) not null,
 Score nvarchar(20) not null);
 
-Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP)
-values ('Kansas City Chiefs', '2019-2020', 'AFC', 'Miami', 'Patrick Mahommes') 
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Kansas City Chiefs', '2019-2020', 'AFC', 'Hard Rock Stadium', 'Patrick Mahommes','31-20') 
 
-Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP)
-values ('New England Patriots', '2018-2019', 'AFC', 'Atlanta', 'Julian Edelman')
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New England Patriots', '2018-2019', 'AFC', 'Mercedez-Benz Stadium', 'Julian Edelman', '13-3')
 
-Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP)
-values ('Phildelphia Eagles', '2017-2018', 'NFC', 'Minnieapolis', 'Nick Foles')
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Phildelphia Eagles', '2017-2018', 'NFC', 'U.S Bank Stadium', 'Nick Foles', '41-33')
 
-Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP)
-values ('New England Patriots', '2016-2017', 'AFC', 'Houston', 'Tom Brady')
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New England Patriots', '2016-2017', 'AFC', 'NRG Stadium', 'Tom Brady', '34-28')
 
-Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP)
-values ('Denver Broncos', '2015-2016', 'AFC', 'Santa Clara', 'Von Miller')
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Denver Broncos', '2015-2016', 'AFC', 'Levi Stadium', 'Von Miller', '24-10')
 
-Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP)
-values ('New England Patriots', '2014-2015', 'AFC', 'Phoenix', 'Tom Brady')
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New England Patriots', '2014-2015', 'AFC', 'University of Phoenix Stadium', 'Tom Brady', '28-24')
 
-Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP)
-values ('Seattle Seahawks', '2013-2014', 'NFC', 
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Seattle Seahawks', '2013-2014', 'NFC', 'MetLife Stadium', 'Malcolm Smith','43-8')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Baltimore Ravens', '2012-2013', 'AFC', 'Mercedez-Benz Superdome', 'Joe Flacco','34-31')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New York Giants', '2011-2012', 'NFC', 'Lucas Oil Stadium', 'Eli Manning','21-17')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Green Bay Packers', '2010-2011', 'NFC', 'Cowboys Stadium', 'Aaron Rodgers','31-25')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New Orleans Saints', '2009-2010', 'NFC', 'Sun Life Stadium', 'Drew Brees')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Pittsburgh Steelers', '2008-2009', 'AFC', 'Raymond James Stadium', 'Santonio Holmes')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New York Giants', '2007-2008', 'NFC', 'University of Phoenix Stadium', 'Eli Manning')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Indianapolis Colts', '2006-2007', 'AFC', 'Dolphin Stadium', 'Peyton Manning')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Pittsburgh Steelers', '2005-2006', 'AFC', 'Ford Field Stadium', 'Hines Ward')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New England Patriots', '2004-2005', 'AFC', 'Alltel Stadium', 'Deion Branch')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New England Patriots', '2003-2004', 'AFC', 'Reliant Stadium', 'Tom Brady')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Tampa Bay Buccaneers', '2002-2003', 'NFC', 'Qualcomm Stadium', 'Dexter Jackson')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New England Patriots', '2001-2002', 'AFC', 'Louisiana Superdome', 'Tom Brady')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Baltimore Ravens', '2000-2001', 'AFC', 'Raymond James Stadium', 'Ray Lewis')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('St. Louis Rams', '1999-2000', 'NFC', 'Georgia Dome', 'Kurt Warner')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Denver Broncos', '1998-1999', 'AFC', 'Pro Player Stadium', 'John Elway')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Denver Broncos', '1997-1998', 'AFC', 'Qualcomm Stadium', 'Terrell Davis')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Green Bay Packers', '1996-1997', 'NFC', 'Louisana Superdome', 'Desmond Howard')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Dallas Cowboys', '1995-1996', 'NFC', 'Sun Devil Stadium', 'Larry Brown')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('San Francisco 49ers', '1994-1995', 'NFC', 'Joe Robbie Stadium', 'Steve Young')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Dallas Cowboys', '1993-1994', 'NFC', 'Georgia Dome', 'Emmitt Smith')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Dallas Cowboys', '1992-1993', 'NFC', 'Rose Bowl', 'Troy Aikmen')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Washington Redskins', '1991-1992', 'NFC', 'Metro Dome', 'Mark Rypien')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New York Giants', '1990-1991', 'NFC', 'Tampa Stadium', 'Ottis Anderson')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('San Francisco 49ers', '1989-1990', 'NFC', 'Louisiana Superdome', 'Joe Montana')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('San Francisco 49ers', '1988-1989', 'NFC', 'Joe Robbie Stadium', 'Jerry Rice')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Washington Redskins', '1987-1988', 'NFC', 'Jack Murphy Stadium', 'Doug Williams')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('New York Giants', '1986-1987', 'NFC', 'Rose Bowl', 'Phil Simms')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Chicago Bears','1985-1986','NFC','Louisiana Superdome','Richard Dent')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('San Francisco 49ers','1984-1985','NFC','Stanford Stadium','Joe Montana')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
+values ('Los Angeles Raiders','1983-1984','AFC','Tampa Stadium','Marcus Allen')
+
+Insert into SuperBowlWinners(Teamname, Year, Division, Location, SuperbowlMVP, Score)
